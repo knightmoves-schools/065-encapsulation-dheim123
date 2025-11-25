16ed0794cee3330c6859e4322d5e391d17821b8f
dheim123 <dheim@midland.k12.ia.us>
2025-11-25 10:36:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+class Rectangle {
+    constructor(length, height) {
+        this.length = length;
+        this.height = height;
+    }
 
-let length = 5;
-let height = 10;
-
-function calculateArea(length, height){
-    return length * height;
+    calculateArea() {
+        return this.length * this.height;
+    }
 }
+
